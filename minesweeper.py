a0fe702481a9e18df2dbb4024f529208a735d2c4
KubykSerhii <41287976+KubykSerhii@users.noreply.github.com>
2018-08-05 21:27:24 +0300

    Update minesweeper.py

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from random import randrange as rand
 
+
 class MainScreen:
     def __init__(self, screen):
         self.min_columns = 9
@@ -13,6 +14,7 @@ class MainScreen:
         self.bombs = 10
         self.fields = []
         self.size_field = 30
+        self.game_over = False
         self.canvas = None
         self.screen = screen
         self.screen.title('Minesweeper by Serhii Kubyk')
@@ -49,6 +51,7 @@ class MainScreen:
             self.canvas.create_line(0, i*(self.size_field+1)+2, self.columns*(self.size_field+1)+2, i*(self.size_field+1)+2)
 
     def creeateNewGame(self):
+        self.game_over = False
         self.checkOnDigit(self.entry_rows)
         self.rows = min(max(int(self.entry_rows.get()), self.min_rows), self.max_rows)
         self.entry_rows.delete(0, END)
@@ -72,25 +75,50 @@ class MainScreen:
         while curr_bombs > 0:
             position = rand(self.columns*self.rows)
             field = self.fields[position // self.columns][position % self.columns]
-            if not field.isBomb:
+            if not field.is_bomb:
                 field.drawBomb()
                 curr_bombs -= 1
         # Calculate neighbour bombs
         for col in self.fields:
             for field in col:
-                if field.isBomb:
+                if field.is_bomb:
                     for pos in range(8):
                         n_field = self.getNeighbourField(field, pos)
-                        if field is not n_field and not n_field.isBomb:
+                        if field is not n_field and not n_field.is_bomb:
                             n_field.neighbour_Bombs += 1
         for col in self.fields:
             for field in col:
                 field.drawCountNeighbourBombs()
 
-
-
     def onClick(self, row, column):
-        self.fields[row][column].frame.destroy()
+        if self.game_over:
+            return None
+        empty_fields = []
+        field = self.fields[row][column]
+        field.show()
+        if field.is_bomb:
+            self.game_over = True
+            field.drawActiveBomb()
+            MessageWindow('bomb')
+            return None
+        if field.neighbour_Bombs == 0:
+            empty_fields.append(field)
+            while empty_fields:
+                field = empty_fields.pop()
+                for pos in range(8):
+                    neighbour_field = self.getNeighbourField(field, pos)
+                    if neighbour_field is not field and not neighbour_field.opened:
+                        neighbour_field.show()
+                        if neighbour_field.neighbour_Bombs == 0:
+                            empty_fields.append(neighbour_field)
+        count_closed_fields = 0
+        for item in self.fields:
+            for field in item:
+                if not field.opened:
+                    count_closed_fields += 1
+        if count_closed_fields == self.bombs:
+            self.game_over = True
+            MessageWindow('win')
 
     def checkOnDigit(self, widget):
         if not all([c in '0123456789' for c in widget.get()]):
@@ -138,20 +166,27 @@ class Field:
         self.y = row*(self.size+1)+3
         self.main_screen = main_screen
         self.canvas = main_screen.canvas
-        self.isBomb = False
+        self.is_bomb = False
+        self.opened = False
+        self.flag = 0
         self.neighbour_Bombs = 0
         self.drawFrame()
 
     def drawFrame(self):
         self.frame = Frame(self.main_screen.canvas, width=self.size, height=self.size, relief='raised', borderwidth=3)
         self.frame.place(x=self.x, y=self.y)
-        self.frame.bind('<Button-1>', lambda x: main_screen.onClick(self.row, self.column))
+        self.frame.bind('<Button-1>', lambda x: self.main_screen.onClick(self.row, self.column))
+        self.frame.bind('<Button-3>', self.drawFlag)
 
     def drawBomb(self):
-        self.isBomb = True
+        self.is_bomb = True
         self.canvas.create_oval(self.x+self.size//3, self.y+self.size//3, self.x+self.size//3*2, self.y+self.size//3*2, fill='black')
         self.canvas.create_line(self.x+self.size//5, self.y+self.size//5, self.x+self.size//5*4, self.y+self.size//5*4, width=1)
         self.canvas.create_line(self.x+self.size//5*4, self.y+self.size//5, self.x+self.size//5, self.y+self.size//5*4, width=1)
+
+    def drawActiveBomb(self):
+        self.canvas.create_rectangle(self.x, self.y, self.x+self.size, self.y+self.size, fill='red4')
+        self.drawBomb()
 
     def drawCountNeighbourBombs(self):
         colors = ['blue', 'dark green', 'red', 'purple4', 'brown4', 'navy', 'yellow', 'dark violet']
@@ -161,11 +196,35 @@ class Field:
                                     text=self.neighbour_Bombs,
                                     fill=colors[self.neighbour_Bombs-1],
                                     font='Times ' + str(self.size//2) + ' bold')
+    def drawFlag(self, event):
+        self.flag = not self.flag
+        if self.flag:
+            self.flag_canvas = Canvas(self.frame, width=self.size-10, height=self.size-10)
+            self.flag_canvas.place(x=0, y=0)
+            self.flag_canvas.create_line(self.size//6, self.size//10, self.size//6, self.size-self.size//5)
+            self.flag_canvas.create_rectangle(self.size//6, self.size//10, self.size-self.size//3, self.size//2, fill='coral')
+            self.flag_canvas.bind('<Button-3>', self.drawFlag)
+        else:
+            self.flag_canvas.destroy()
+
+    def show(self):
+        self.frame.destroy()
+        self.opened = True
+
+
+class MessageWindow(Toplevel):
+    def __init__(self, result):
+        super().__init__(root)
+        self.title('Message Window')
+        self.geometry('250x100')
+        self.resizable(False, False)
+        self.grab_set()
+        self.focus_set()
+        self.canvas = Canvas(self, width=250, height=100, bg='grey76')
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_text(120, 40, text='You win!!!' if result == 'win' else 'You lose!!!', fill='blue', font='Times 20 bold')
 
 
 root = Tk()
-main_screen = MainScreen(root)
+MainScreen(root)
 root.mainloop()
-
-
-
